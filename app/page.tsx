49a6ebd6a0f3a69fdfb9e6978070f6afc8d1184f
Artur Campos <arturcamposba@gmail.com>
2024-05-06 22:50:02 -0300

    feat: add category item and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import SearchInput from "./_components/ui/search-input";
 
@@ -7,6 +8,10 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <SearchInput onSearch={() => {}} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
       </div>
     </>
   );
