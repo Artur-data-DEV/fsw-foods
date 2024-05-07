255213cca49b1802fd6d31e149b73ee9cf5f6373
Artur Campos <arturcamposba@gmail.com>
2024-05-06 23:13:13 -0300

    feat: add promo-banner

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -6,14 +6,14 @@ interface CategoryItemProps {
 }
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
+    <div className="flex min-w-[160px] items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
       <Image
         src={category.imageUrl}
         alt={category.name}
         height={30}
         width={30}
       />
-      <span className="text-sm font-semibold">{category.name}</span>
+      <p className="block text-sm font-semibold">{category.name}</p>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,33 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import SearchInput from "./_components/ui/search-input";
 
 const Home = () => {
+  const cn = "px-5 pt-6";
   return (
     <>
       <Header />
-      <div className="px-5 pt-6">
+      <div className={cn}>
         <SearchInput onSearch={() => {}} />
       </div>
 
-      <div className="px-5 pt-6">
+      <div className={cn}>
         <CategoryList />
+      </div>
+
+      <div className={cn}>
+        <Image
+          src="/Banner_Pizza.png"
+          alt="Ate 30% de desconto em pizzas!"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+          priority={true}
+          layout="responsive"
+        />
       </div>
     </>
   );
